44d55328efe9c8adc2813aa39f63fdeba74db3a5
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-12-18 14:18:45 -0500

    test: add a basic test

---
+++ __tests__/test.ts
@@ -0,0 +1,9 @@
+// SPDX-FileCopyrightText: 2024 Ali Sajid Imami
+//
+// SPDX-License-Identifier: MIT
+
+describe("Test tests", () => {
+    it('should return true', () => {
+        expect(true).toBe(true)
+    })
+})
